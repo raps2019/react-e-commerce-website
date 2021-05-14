ee87a85766a28eaeddfab29b9e89264bfd8bba84
Douglas Colaco <douglascolaco@gmail.com>
2021-05-14 00:13:45 -0600

    added home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,20 @@
 import React, {Fragment} from 'react'
+import shopSpeakers from '../images/homepage/shopSpeakers.jpg'
+import shopAmplifiers from '../images/homepage/shopAmplifiers.png'
 
 const Home = () => {
   return (
     <Fragment>
-      <h1>Home</h1>
-      <p className="about__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. voluptates facilis eum. Quidem.</p>
-      <p className="about__paragraph">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti consequatur mollitia voluptates rerum laborum aspernatur sit quasi. </p>
-      <p className="about__paragraph">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quaerat, non qui perferendis eligendi iusto laborum harum molestiae saepe aliquam! Nulla, adipisci assumenda. Temporibus modi quae earum provident ad, ipsum assumenda optio atque obcaecati blanditiis asperiores quam et necessitatibus rerum, laboriosam eveniet autem! Pariatur ex minus aliquam quam rem praesentium repudiandae at rerum, repellat dignissimos consequatur sunt similique aliquid quos distinctio dicta, eveniet assumenda amet fuga dolore? Doloremque, dolorum dolorem.</p>
-      <p className="about__paragraph">Qui voluptatem magnam deleniti quibusdam. Vitae perferendis hic minima commodi omnis. Impedit sed recusandae suscipit, excepturi nobis eligendi inventore, cupiditate modi, culpa nam fugiat. Quod perferendis, numquam at maiores omnis ipsam incidunt molestias assumenda cumque adipisci totam error voluptate laborum illo non, facere praesentium pariatur consequuntur et! Iste corrupti officia ut id voluptate quasi deleniti ratione! Architecto maiores assumenda veniam deserunt nesciunt ipsum voluptatem!</p>
+      <div className="container__home-page">
+        <div className='container__shop-category'>
+          <h1 className="text--image-centered">Shop Speakers</h1>
+          <img className="image--shop-category" src={shopSpeakers}></img>
+        </div>
+        <div className='container__shop-category'>
+          <h1 className="text--image-centered">Shop Amplifiers</h1>
+          <img className="image--shop-category" src={shopAmplifiers}></img>
+        </div>
+      </div>      
     </Fragment>
   )
 }
